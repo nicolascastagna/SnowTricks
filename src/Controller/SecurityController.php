59dd9ebefaa1c0e0a1a3f622aa5ecac84a33ed17
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2024-05-04 17:42:41 +0200

    add style css form login and register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,4 +29,10 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/', name: 'homepage')]
+    public function index(): Response
+    {
+        return new Response('test');
+    }
 }
