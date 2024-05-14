3efb00a614a3932db9a5a7dc37f402fb860a8a7d
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2024-05-14 21:15:02 +0200

    add route forgotten password with link email

--- src/Form/ResetPasswordFormType.php
+++ src/Form/ResetPasswordFormType.php
@@ -2,40 +2,22 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class RegistrationFormType extends AbstractType
+class ResetPasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', TextType::class, [
-                'label' => 'Nom d\'utilisateur',
+            ->add('password', PasswordType::class, [
+                'label' => 'Entrez votre mot de passe',
                 'attr' => [
-                    'placeholder' => 'Entrez votre nom d\'utilisateur'
-                ]
-            ])
-            ->add('email', EmailType::class, [
-                'label' => 'Adresse email',
-                'attr' => [
-                    'placeholder' => 'Entrez votre adresse email'
-                ]
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => [
-                    'autocomplete' => 'new-password',
-                    'placeholder' => 'Entrez un mot de passe'
+                    'placeholder' => 'Entrez un mot de passe',
                 ],
                 'constraints' => [
                     new NotBlank([
@@ -53,8 +35,6 @@ class RegistrationFormType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
+        $resolver->setDefaults([]);
     }
 }
