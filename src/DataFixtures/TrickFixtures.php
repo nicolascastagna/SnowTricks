12d253a322fbf0df0a872ac85b904000ca19b9b2
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2024-06-21 11:32:44 +0200

    load fixtures images user with suffix and cleaner directory

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -33,32 +33,32 @@ class TrickFixtures extends Fixture
         $users = $manager->getRepository(User::class)->findAll();
         $categories = $manager->getRepository(Category::class)->findAll();
 
-        for ($i = 0; $i < 14; $i++) {
-            $trick = new Trick();
-            $trick->setName($faker->words(3, true));
-            $trick->setDescription($faker->paragraph());
-            $trick->setCreationDate($faker->dateTimeBetween('-1 year', 'now'));
+        // for ($i = 0; $i < 14; $i++) {
+        //     $trick = new Trick();
+        //     $trick->setName($faker->words(3, true));
+        //     $trick->setDescription($faker->paragraph());
+        //     $trick->setCreationDate($faker->dateTimeBetween('-1 year', 'now'));
 
-            $trick->setUser($faker->randomElement($users));
-            $trick->setCategory($faker->randomElement($categories));
+        //     $trick->setUser($faker->randomElement($users));
+        //     $trick->setCategory($faker->randomElement($categories));
 
-            $mainImage = $faker->randomElement($trickMainImages);
-            $trick->setMainImage($mainImage);
+        //     $mainImage = $faker->randomElement($trickMainImages);
+        //     $trick->setMainImage($mainImage);
 
-            // for ($j = 0; $j < $faker->numberBetween(1, 3); $j++) {
-            //     $picture = new Picture();
-            //     $picture->setName($faker->randomElement($trickImages));
-            //     $trick->addPicture($picture);
-            // }
+        // for ($j = 0; $j < $faker->numberBetween(1, 3); $j++) {
+        //     $picture = new Picture();
+        //     $picture->setName($faker->randomElement($trickImages));
+        //     $trick->addPicture($picture);
+        // }
 
-            // for ($j = 0; $j < $faker->numberBetween(1, 2); $j++) {
-            //     $video = new Video();
-            //     $video->setName($faker->randomElement($videoUrls));
-            //     $trick->addVideo($video);
-            // }
+        // for ($j = 0; $j < $faker->numberBetween(1, 2); $j++) {
+        //     $video = new Video();
+        //     $video->setName($faker->randomElement($videoUrls));
+        //     $trick->addVideo($video);
+        // }
 
-            $manager->persist($trick);
-        }
+        //     $manager->persist($trick);
+        // }
 
         $manager->flush();
     }
